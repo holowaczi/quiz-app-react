2b6a1d1963a0f0c6673acc59ee291c00234a21a1
holowaczi <igor.holowacz@gmail.com>
2023-11-17 07:58:31 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const questions = [
+  {
+    question: 'What is the capital of France?',
+    options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+    correctAnswer: 'Paris',
+  },
+  {
+    question: 'Which programming language is this app built with?',
+    options: ['Java', 'Python', 'JavaScript', 'C++'],
+    correctAnswer: 'JavaScript',
+  },
+  {
+    question: 'What is the largest planet in our solar system?',
+    options: ['Earth', 'Jupiter', 'Mars', 'Venus'],
+    correctAnswer: 'Jupiter',
+  },
+];
+
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleAnswerClick = (selectedOption) => {
+    setSelectedOption(selectedOption);
+
+    if (selectedOption === questions[currentQuestion].correctAnswer) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setTimeout(() => {
+        setCurrentQuestion(nextQuestion);
+        setSelectedOption(null); // Reset selected option for the next question
+      }, 1000);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const restartQuiz = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          <h2>Your Score: {score}</h2>
+          <button onClick={restartQuiz}>Restart Quiz</button>
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <h2>Question {currentQuestion + 1}</h2>
+            <p>{questions[currentQuestion].question}</p>
+          </div>
+          <div className="answer-section">
+        {questions[currentQuestion].options.map((option, index) => (
+          <button
+            key={index}
+            onClick={() => handleAnswerClick(option)}
+            className={
+              showScore
+                ? option === questions[currentQuestion].correctAnswer
+                  ? 'correct'
+                  : 'incorrect'
+                : ''
+            }
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+        </>
+      )}
     </div>
   );
 }
 
 export default App;
+
